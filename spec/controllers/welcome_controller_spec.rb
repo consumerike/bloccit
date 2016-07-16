bd3d117fbd0fde6ef875af5d59a2e0b46b9036c3
consumerike <consumerike@gmail.com>
2016-07-16 16:40:33 +0000

    updated checkpoint26

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -9,11 +9,13 @@ RSpec.describe WelcomeController, type: :controller do
        expect(response).to render_template("index")
      end
    end
- end
+ 
 
- describe "GET about" do
+
+   describe "GET about" do
      it "renders the about template" do
        get :about
        expect(response).to render_template("about")
      end
    end  
+end
